d2a0c15676c1d12b704e630b230d8c9349888412
BrandMineGamer079 <165478025+BrandMineGamer079@users.noreply.github.com>
2025-04-29 21:04:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
     mongoURI : "mongodb+srv://codex-in1:codex-in1@codex-in1.1bgxp.mongodb.net/?retryWrites=true&w=majority",
     statusType: 3, // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom
     statusContent: "A", // A = Show total users count, B = Show total servers count, C = Custom message
-    customStatus: "GlaceYT",
-    streamingURL: "https://www.twitch.tv/glaceytt"
+    customStatus: "Team Sanatan",
+    streamingURL: "http://www.youtube.com/@brandminegamer7"
 };
